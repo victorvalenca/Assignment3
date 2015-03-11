453a73faf14ad37587e15af388c0973e3700a3ac
Victor Fernandes <victorvalenca@gmail.com>
2015-03-11 17:57:57 -0400

    Initial program stub

---
+++ src/main.c
@@ -0,0 +1,26 @@
+/*
+ * TITLE: SUBSTITUTION CIPHER
+ *
+ * PURPOSE: To decypher a string using frequency analysis
+ *
+ * Created by Victor Fernandes March 26, 2015
+ *
+*/
+
+/**************************************
+ *
+ * Begin import statements
+ *
+**************************************/
+#include <stdio.h>
+
+
+int main(){
+
+	char[] cipherText = "INSERT CIPHER HERE.";
+	int[26] charFrequency = {0};
+	int* alphaSort[26] = {0};
+
+
+
+}
